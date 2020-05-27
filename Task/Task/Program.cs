40e25ce3535d8544963fdaa4c5fc0956d5f64bd2
Sandra Marszałek <sandra.marszalek1310@gmail.com>
2020-05-27 12:40:37 +0200

    wgranie jeszcze raz po mailu Pani Diany Wołek

---
+++ Task/Task/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Task
+{
+    class Zabawa
+    { 
+        private const double w = 1.5;
+        int h = 0;
+        double b = 0.0;
+
+        
+
+        public int Gra(int h, double b)
+        {
+            if (h < 1 || b < 0 || b >=1 )
+            {
+                return -1;
+            }
+            var widoczna = 1;
+
+            for (double aktualna_wys = h; aktualna_wys >= widoczna;)
+            {
+                aktualna_wys *= b;
+                widoczna += 2; 
+            }
+            return widoczna;
+
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task/Task/Zabawa.Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Task
+{
+    [TestFixture]
+    class ZabawaTest
+    {
+        [TestCase]
+        public void Test()
+        {
+            Zabawa z = new Zabawa();
+            Assert.AreEqual(3, z.Gra(3, 0.66));
+        }
+
+        [TestCase]
+        public void Test2()
+        {
+            Zabawa z = new Zabawa();
+            Assert.AreEqual(-1, z.Gra(3, 1));
+        }
+
+
+    }
+}
